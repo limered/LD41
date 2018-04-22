68eab1cae938585e118aafa8b304554c8e8844fd
Sven Milker <s.milker@brickmakers.de>
2018-04-22 15:36:56 +0200

    added BoxCollider2D as required component for WallComponent

--- LD41/Assets/Systems/Interaction/Walls/WallComponent.cs
+++ LD41/Assets/Systems/Interaction/Walls/WallComponent.cs
@@ -1,7 +1,9 @@
 using SystemBase;
+using UnityEngine;
 
 namespace Systems.Interaction.Walls
 {
+    [RequireComponent(typeof(BoxCollider2D))]
     public class WallComponent : GameComponent
     {
     }
