4c28b04354ffd815dfacf871880df3936fd98358
Sven Milker <s.milker@brickmakers.de>
2018-04-22 17:53:26 +0200

    added billboard feature to trees

---
+++ LD41/Assets/Billboard.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class Billboard : MonoBehaviour
+    {
+        void Update()
+        {
+            var dir = Camera.main.transform.up;
+            transform.up = dir;
+        }
+    }
+}
